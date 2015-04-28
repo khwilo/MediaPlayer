d392ebbf07534ce12bce105925968a688003d3cf
Khwilo Kabaka <kwhyzzy@gmail.com>
2015-04-29 02:04:03 +0300

    add menu items and set the menu bar to the top of the player

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,9 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -11,10 +14,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage){
+
+        //add the menu items
+        MenuItem open = new MenuItem("Open");
+        Menu file = new Menu("File");
+        MenuBar menu = new MenuBar();
+
+        //add the items to each other
+        file.getItems().add(open);
+        menu.getMenus().add(file);
+
         //instantiate Player class
         Player player = new Player("file:///E:/ProgrammingVideos/JohnLegend-Success.mp4");
+        //add the menu to the player
+        player.setTop(menu);
         //add the player to the scene
-        Scene scene = new Scene(player, 720, 510, Color.BLACK);
+        Scene scene = new Scene(player, 720, 535, Color.BLACK);
         //adding the scene to the stage
         primaryStage.setScene(scene);
         //show the stage
